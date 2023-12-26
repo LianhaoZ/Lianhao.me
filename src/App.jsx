12f8fcc82c0c590fef4a7ac54e5cf6378568a75b
Lianhao Zheng <lianhaoz06@gmail.com>
2023-12-26 13:50:47 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import{ About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from "./components";
+function App() {
+  const [count, setCount] = useState(0)
 
-const App = () => { 
   return (
-    <BrowserRouter>
-    
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar/>
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        {/* <Feedbacks /> */}
-        <div className = "relative z-0">
-          <Contact />
-        </div>
-        <StarsCanvas />
-      </div>
-    </BrowserRouter>
+    <>
+    </>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
+import { presetUno, presetAttributify, presetIcons} from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    UnoCSS({presets: [presetUno(), presetAttributify(), presetIcons()]}),
+  ],
 })
