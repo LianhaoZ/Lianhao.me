d832c461d765a4b00ef8ecc315513d4c07519c81
Lianhao Zheng <lianhaoz06@gmail.com>
2023-12-30 14:33:37 -0500

    Update index.js

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Load from './Load';
-import StarsCanvas from './Stars'; 
+import StarsCanvas from './Stars';
+import Footer from './footer';
 
 
-export {Navbar, Load, StarsCanvas};
+export {Navbar, Load, StarsCanvas, Footer};
