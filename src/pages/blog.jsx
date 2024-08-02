c5e9942c2288e755678e358a6dd04fa4caa2b4cd
Lianhao Zheng <lianhaoz06@gmail.com>
2024-08-02 18:45:35 -0400

    Update blog.jsx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
 
     
     return(
-        <>
+        <> 
         {/* <div className='prose m-auto'>
             <h1>
                 <div className=' text-gray-200 justify-center pt-2 lt-sm:text-size-0.75em'>
